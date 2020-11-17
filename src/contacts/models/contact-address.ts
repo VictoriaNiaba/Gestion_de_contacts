d652a8406bf1fe21e29b038e2b78ef18d39c3952
Hafsa FARHANE LAACHIRI <farhane.hafsa@gmail.com>
2020-11-17 10:07:58 +0100

    model contact est fait !

---
+++ src/contacts/models/contact-address.ts
@@ -0,0 +1,7 @@
+export class ContactAddress {
+  street1: string;
+  street2: string;
+  postalCode: string;
+  city: string;
+  country: string;
+}
--- src/contacts/models/contact.ts
+++ src/contacts/models/contact.ts
@@ -1,7 +1,12 @@
-export class Contact{
-    id: number;
-    civility: string;
-    firstName: string;
-    lastName: string;
-    email: string;
+import { ContactAddress } from './contact-address';
+import { PhoneNumber } from './phone-number';
+
+export class Contact {
+  id: number;
+  civility: Civility;
+  firstName: string;
+  lastName: string;
+  email: string;
+  address: ContactAddress;
+  phoneNumber: PhoneNumber[];
 }
