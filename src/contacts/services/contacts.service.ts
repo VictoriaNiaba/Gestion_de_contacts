2571f587a52bcc154297762db0dda50896283ee1
Hafsa FARHANE LAACHIRI <farhane.hafsa@gmail.com>
2020-11-18 19:52:13 +0100

    Correction du fichier ESCLinst

--- src/contacts/services/contacts.service.ts
+++ src/contacts/services/contacts.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { CreateContactDto } from '../dto/create-contact.dto';
 import { UpdateContactDto } from '../dto/update-contact.dto';
@@ -6,44 +6,57 @@ import { Contact } from '../entities/contact.entity';
 
 @Injectable()
 export class ContactsService {
-  constructor(
-    @Inject('CONTACTS_REPOSITORY')
-    private contactsRepository: Repository<Contact>,
-  ) {}
+    constructor(
+        @Inject('CONTACTS_REPOSITORY')
+        private contactsRepository: Repository<Contact>,
+    ) {
+    }
 
-  create(createContactDto: CreateContactDto) {
-    return this.contactsRepository.save(createContactDto);
-  }
+    create(createContactDto: CreateContactDto) {
+        if (createContactDto == null) {
+            throw new HttpException({
+                status: HttpStatus.BAD_REQUEST,
+                error: 'Contact can not be null',
+            }, 404);
+        }
+        return this.contactsRepository.save(createContactDto);
+    }
 
-  addContact(createContactDto: CreateContactDto) {
-    return this.contactsRepository.save(createContactDto);
-  }
+    addContact(createContactDto: CreateContactDto) {
+        if (createContactDto == null) {
+            throw new HttpException({
+                status: HttpStatus.BAD_REQUEST,
+                error: 'Contact can not be null',
+            }, 404);
+        }
+        return this.contactsRepository.save(createContactDto);
+    }
 
-  findAll() {
-    return this.contactsRepository.find();
-  }
+    findAll() {
+        return this.contactsRepository.find();
+    }
 
-  findContact() {
-    return this.contactsRepository.findOne();
-  }
+    findContact() {
+        return this.contactsRepository.findOne();
+    }
 
-  findOne(id: number) {
-    return this.contactsRepository.findOne(id);
-  }
+    findOne(id: number) {
+        return this.contactsRepository.findOne(id);
+    }
 
-  update(id: number, updateContactDto: UpdateContactDto) {
-    return this.contactsRepository.update(id, updateContactDto);
-  }
+    update(id: number, updateContactDto: UpdateContactDto) {
+        return this.contactsRepository.update(id, updateContactDto);
+    }
 
-  remove(id: number) {
-    return this.contactsRepository.delete(id);
-  }
+    remove(id: number) {
+        return this.contactsRepository.delete(id);
+    }
 
-  updateContact(id: number, updateContactDto: UpdateContactDto) {
-    return this.contactsRepository.update(id, updateContactDto);
-  }
+    updateContact(id: number, updateContactDto: UpdateContactDto) {
+        return this.contactsRepository.update(id, updateContactDto);
+    }
 
-  removeContact(id: number) {
-    return this.contactsRepository.delete(id);
-  }
+    removeContact(id: number) {
+        return this.contactsRepository.delete(id);
+    }
 }
