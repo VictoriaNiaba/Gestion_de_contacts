438cec3a44d3a6be1e3ede1698a503e7080f575e
VictoriaNiaba <niaba.victoria@gmail.com>
2020-11-19 09:04:57 +0100

    Capture d'erreurs BadRequest dans le service Contact (fait par Hafsa)
    Fait par Hafsa dans la branche "models / services". Migré ici pour pouvoir travailler sur la fonctionnalité des exceptions en isolation.

--- src/contacts/services/contacts.service.ts
+++ src/contacts/services/contacts.service.ts
@@ -1,8 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
-
 import { Contact } from '../entities/contact.entity';
-import { CreateContactDto, UpdateContactDto } from "../dto";
+import * as _ from 'lodash';
+import { ApiError, CreateContactDto, UpdateContactDto } from "../dto";
 
 @Injectable()
 export class ContactsService {
@@ -13,23 +13,45 @@ export class ContactsService {
     }
 
     create(createContactDto: CreateContactDto) {
+        if (_.isEmpty(createContactDto)) {
+            const error: ApiError = {
+                status: HttpStatus.BAD_REQUEST,
+                title: 'Error while saving contact',
+                message: 'Contact cannot be null or empty',
+                date: new Date()
+            };
+            throw new HttpException(error, 404);
+        }
         return this.contactsRepository.save(createContactDto);
     }
+
 
     findAll() {
         return this.contactsRepository.find();
     }
 
-    findOne(id: number) {
-        return this.contactsRepository.findOne(id);
+    async findOne(id: number) {
+        const contact: Contact = await this.contactsRepository.findOne(id);
+        if (contact == null) {
+            const error: ApiError = {
+                status: HttpStatus.BAD_REQUEST,
+                title: 'Error while looking for contact',
+                message: 'Contact cannot be found',
+                date: new Date()
+            };
+            throw new HttpException(error, 404);
+        }
+        return contact;
     }
 
     update(id: number, updateContactDto: UpdateContactDto) {
+        this.findOne(id);
         return this.contactsRepository.update(id, updateContactDto);
     }
 
     remove(id: number) {
         return this.contactsRepository.delete(id);
     }
+
 
 }
