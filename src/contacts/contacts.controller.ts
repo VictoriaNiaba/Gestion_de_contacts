20dc1a1b88e9037f57104895d730e3029202e566
VictoriaNiaba <niaba.victoria@gmail.com>
2020-11-19 17:47:55 +0100

    Corrections grammaire / orthographe de la documentation des routes

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -1,51 +1,66 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
-import { from } from 'rxjs';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ContactsService } from './contacts.service';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
-import {ApiTags,ApiOkResponse, ApiCreatedResponse,ApiInternalServerErrorResponse,ApiOperation}from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOkResponse,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
 
 @ApiTags('contacts')
 @Controller('contacts')
 export class ContactsController {
   constructor(private readonly contactsService: ContactsService) {}
 
-  
-  @ApiOperation({summary: 'Lister les contacts'})
-  @ApiOkResponse({description: ' Contacts trouvées '})
-  @ApiInternalServerErrorResponse ( {status: 500,description: 'Erreur interne du serveur'})
+  @ApiOperation({ summary: 'Lister les contacts' })
+  @ApiOkResponse({ description: ' Contacts trouvés ' })
+  @ApiInternalServerErrorResponse({
+    status: 500,
+    description: 'Erreur interne du serveur',
+  })
   @Get()
   findAll() {
     return this.contactsService.findAll();
   }
 
-  @ApiOperation({summary: 'Créer un contact'})
-  @ApiOkResponse({description: 'Contact crée avec succès'})
-  @ApiInternalServerErrorResponse ( {description: 'Erreur interne du serveur'})
+  @ApiOperation({ summary: 'Créer un contact' })
+  @ApiCreatedResponse({ description: 'Contact créé avec succès' })
+  @ApiInternalServerErrorResponse({ description: 'Erreur interne du serveur' })
   @Post()
   create(@Body() createContactDto: CreateContactDto) {
     return this.contactsService.create(createContactDto);
   }
 
-  @ApiOperation({summary: 'Trouver un contact '})
-  @ApiCreatedResponse({description: 'Contact trouvée '})
-  @ApiInternalServerErrorResponse ( {description: 'Erreur interne du serveur'})
+  @ApiOperation({ summary: 'Trouver un contact' })
+  @ApiOkResponse({ description: 'Contact trouvé' })
+  @ApiInternalServerErrorResponse({ description: 'Erreur interne du serveur' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.contactsService.findOne(+id);
   }
 
-  @ApiOperation({summary: 'Modifier un contact '})
-  @ApiOkResponse({description: 'Mise à jour du contact '})
-  @ApiInternalServerErrorResponse ( {description: 'Erreur interne du serveur'})
+  @ApiOperation({ summary: 'Modifier un contact' })
+  @ApiOkResponse({ description: 'Contact mis à jour' })
+  @ApiInternalServerErrorResponse({ description: 'Erreur interne du serveur' })
   @Put(':id')
   update(@Param('id') id: string, @Body() updateContactDto: UpdateContactDto) {
     return this.contactsService.update(+id, updateContactDto);
   }
 
-  @ApiOperation({summary: 'Suprimer un contact '})
-  @ApiOkResponse({description: 'Contact supprimé'})
-  @ApiInternalServerErrorResponse ( { description: 'Erreur interne du serveur'})
+  @ApiOperation({ summary: 'Supprimer un contact' })
+  @ApiOkResponse({ description: 'Contact supprimé' })
+  @ApiInternalServerErrorResponse({ description: 'Erreur interne du serveur' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.contactsService.remove(+id);
