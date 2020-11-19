309fb7c3352c7d5d5d6c463438f062f858932f85
MohamedOmar <medyakiziomar@gmail.com>
2020-11-19 02:39:44 +0100

    Configuration OpenApi et realisation de la première version de la doc

--- src/contacts/dto/create-contact.dto.ts
+++ src/contacts/dto/create-contact.dto.ts
@@ -1,4 +1,9 @@
+import { from } from "rxjs";
+import {ApiProperty} from '@nestjs/swagger';
+
 export class CreateContactDto {
+    @ApiProperty()
     firstName: string;
+    @ApiProperty()
     lastName: string;
 }
--- src/contacts/entities/contact.entity.ts
+++ src/contacts/entities/contact.entity.ts
@@ -10,4 +10,5 @@ export class Contact {
 
   @Column()
   lastName: string;
+  
 }
