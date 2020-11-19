58b654c782cc1c6a0b4dd2e651a6a815bc2d8300
MohamedOmar <medyakiziomar@gmail.com>
2020-11-19 17:25:53 +0100

    Modification des contacts contrôleurs pour le doc OpenApi

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -3,7 +3,7 @@ import { from } from 'rxjs';
 import { ContactsService } from './contacts.service';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
-import {ApiTags,ApiOkResponse, ApiCreatedResponse,ApiNotFoundResponse,ApiInternalServerErrorResponse,ApiForbiddenResponse} from '@nestjs/swagger';
+import {ApiTags,ApiOkResponse, ApiCreatedResponse,ApiInternalServerErrorResponse,ApiOperation}from '@nestjs/swagger';
 
 @ApiTags('contacts')
 @Controller('contacts')
@@ -11,47 +11,41 @@ export class ContactsController {
   constructor(private readonly contactsService: ContactsService) {}
 
   
-  
-  @ApiOkResponse({status: 202,description: 'Contact récupérées par ID avec succès'})
-  @ApiForbiddenResponse({description:'Forbidden'})
-  @ApiNotFoundResponse({description: 'Aucune tâche trouvée pour l ID'})
-  @ApiInternalServerErrorResponse ( {status: 402,description: 'Erreur interne du serveur'})
+  @ApiOperation({summary: 'Lister les contacts'})
+  @ApiOkResponse({description: ' Contacts trouvées '})
+  @ApiInternalServerErrorResponse ( {status: 500,description: 'Erreur interne du serveur'})
   @Get()
   findAll() {
     return this.contactsService.findAll();
   }
 
-  @ApiOkResponse({status: 202,description: 'Contact crée avec succès'})
-  @ApiForbiddenResponse({description:'Forbidden'})
-  @ApiNotFoundResponse({description: 'Aucune contact pour l ID'})
-  @ApiInternalServerErrorResponse ( {status: 402,description: 'Erreur interne du serveur'})
+  @ApiOperation({summary: 'Créer un contact'})
+  @ApiOkResponse({description: 'Contact crée avec succès'})
+  @ApiInternalServerErrorResponse ( {description: 'Erreur interne du serveur'})
   @Post()
   create(@Body() createContactDto: CreateContactDto) {
     return this.contactsService.create(createContactDto);
   }
 
-  @ApiCreatedResponse({status: 202,description: 'Contact est crée avec succès'})
-  @ApiForbiddenResponse({description:'Forbidden'})
-  @ApiNotFoundResponse({description: 'Aucune contact trouvée pour l ID'})
-  @ApiInternalServerErrorResponse ( {status: 403,description: 'Erreur interne du serveur'})
+  @ApiOperation({summary: 'Trouver un contact '})
+  @ApiCreatedResponse({description: 'Contact trouvée '})
+  @ApiInternalServerErrorResponse ( {description: 'Erreur interne du serveur'})
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.contactsService.findOne(+id);
   }
 
-  @ApiOkResponse({status: 202,description: 'Contact modifié par ID avec succès'})
-  @ApiForbiddenResponse({description:'Forbidden'})
-  @ApiNotFoundResponse({description: 'Aucune contact à modifier ' })
-  @ApiInternalServerErrorResponse ( {status: 403 , description: 'Erreur interne du serveur'})
+  @ApiOperation({summary: 'Modifier un contact '})
+  @ApiOkResponse({description: 'Mise à jour du contact '})
+  @ApiInternalServerErrorResponse ( {description: 'Erreur interne du serveur'})
   @Put(':id')
   update(@Param('id') id: string, @Body() updateContactDto: UpdateContactDto) {
     return this.contactsService.update(+id, updateContactDto);
   }
 
-  @ApiOkResponse({status: 202,description: 'Contact supprimé par ID avec succès'})
-  @ApiForbiddenResponse({description:'Forbidden'})
-  @ApiNotFoundResponse({description: 'Aucune contact à supprimer ' })
-  @ApiInternalServerErrorResponse ( {status: 403 , description: 'Erreur interne du serveur'})
+  @ApiOperation({summary: 'Suprimer un contact '})
+  @ApiOkResponse({description: 'Contact supprimé'})
+  @ApiInternalServerErrorResponse ( { description: 'Erreur interne du serveur'})
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.contactsService.remove(+id);
