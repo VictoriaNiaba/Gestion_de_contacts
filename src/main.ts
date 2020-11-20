905fe776f32f442edbb250655eb7c94337afa365
VictoriaNiaba <niaba.victoria@gmail.com>
2020-11-20 12:06:36 +0100

    Ajout d'un filtre de validation global

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Filtre global qui applique les règles de validation pour chaque route
+  app.useGlobalPipes(new ValidationPipe());
+  
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
