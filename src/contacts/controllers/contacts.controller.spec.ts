a19bbf036e1428486837a25dcacd6400d7ad622d
Hafsa FARHANE LAACHIRI <farhane.hafsa@gmail.com>
2020-11-22 16:28:51 +0100

    Fix un bug sur controlleur et amélioration des test de controlleur

--- src/contacts/controllers/contacts.controller.spec.ts
+++ src/contacts/controllers/contacts.controller.spec.ts
@@ -1,130 +1,130 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ContactsController } from './contacts.controller';
 import { ContactsService } from '../services/contacts.service';
-import { Repository } from "typeorm";
-import { Civility, Contact } from "../entities";
+import { Civility } from "../entities";
+import { MockType } from "../services/contacts.service.spec";
 
-export type MockType<T> = {
-  [P in keyof T]?: jest.Mock<{}>;
-};
-
-// @ts-ignore
-export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(() => ({
-  findOne: jest.fn(entity => entity),
-  find: jest.fn(entity => entity),
-  update: jest.fn(entity => entity),
-  save: jest.fn(entity => entity),
-  delete: jest.fn(entity => entity),
-
+export const contactsServiceMockFactory: () => MockType<ContactsService> = jest.fn(() => ({
+    findAll: jest.fn(() => []),
+    update: jest.fn((id, entity) => entity),
+    remove: jest.fn((id) => true),
+    create: jest.fn((entity) => true),
+    findOne: jest.fn((entity) => true),
 }));
 
 describe('ContactsController', () => {
-  let controller: ContactsController;
-  let service: ContactsService;
-  let repositoryMock: MockType<Repository<Contact>>;
+    let controller: ContactsController;
+    let service;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [ContactsController],
-      providers: [ContactsService,
-        { provide: 'CONTACTS_REPOSITORY', useFactory: repositoryMockFactory },
-      ],
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [ContactsController],
+            providers: [
+                {
+                    provide: ContactsService,
+                    useFactory: contactsServiceMockFactory
+                }
+            ],
 
-    }).compile();
+        }).compile();
 
-    controller = module.get<ContactsController>(ContactsController);
-    service = module.get<ContactsService>(ContactsService);
-    repositoryMock = module.get('CONTACTS_REPOSITORY');
-  });
-
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-
-  describe('findAll', () => {
-    it('should return an array of contacts', async () => {
-      const result = [{
-          id: 1,
-          firstName: 'test',
-          lastName: 'test',
-          civility: Civility.M,
-          phoneNumber: '094393904350',
-          email: 'deofke@gmail.com',
-          address: null,
-        }];
-      // @ts-ignore
-      jest.spyOn(service, 'findAll').mockImplementation(() => result);
-      expect(await controller.findAll()).toBe(result);
+        controller = module.get<ContactsController>(ContactsController);
+        service = module.get<ContactsService>(ContactsService);
     });
-  });
 
-  describe('create', () => {
-    it('should create a contact', async () => {
-      const contactToCreate = {
-        id: 1,
-        firstName: 'test',
-        lastName: 'test',
-        civility: Civility.M,
-        phoneNumber: '094393904350',
-        email: 'deofke@gmail.com',
-        address: null,
-      };
-      // @ts-ignore
-      jest.spyOn(service, 'create').mockImplementation(() => contactToCreate);
-      expect(await controller.create(contactToCreate)).toBe(contactToCreate);
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
     });
-  });
 
-  describe('findOne', () => {
-    it('should find a contact', async () => {
-      const contactToFind = {
-        id: 1,
-        firstName: 'test',
-        lastName: 'test',
-        civility: Civility.M,
-        phoneNumber: '094393904350',
-        email: 'deofke@gmail.com',
-        address: null,
-      };
-      // @ts-ignore
-      jest.spyOn(service, 'findOne').mockImplementation(() => contactToFind);
-      expect(await controller.findOne(contactToFind.id.toString())).toBe(contactToFind);
+    describe('findAll', () => {
+        it('should return an array of contacts', async () => {
+            const result = [{
+                id: 1,
+                firstName: 'test',
+                lastName: 'test',
+                civility: Civility.M,
+                phoneNumber: '094393904350',
+                email: 'deofke@gmail.com',
+                address: null,
+            }];
+            // @ts-ignore
+            jest.spyOn(service, 'findAll');
+            service.findAll = jest.fn(() => result);
+            expect(await controller.findAll()).toBe(result);
+        });
     });
-  });
 
-  describe('update', () => {
-    it('should update a contact', async () => {
-      const id = 10;
-      const contactToUpdate = {
-        firstName: 'test',
-        lastName: 'test',
-        civility: Civility.M,
-        phoneNumber: '094393904350',
-        email: 'deofke@gmail.com',
-        address: null,
-      };
-      // @ts-ignore
-      jest.spyOn(service, 'update').mockImplementation(() => contactToUpdate);
-      expect(await controller.update(id.toString(),contactToUpdate)).toBe(contactToUpdate);
+    describe('create', () => {
+        it('should create a contact', async () => {
+            const contactToCreate = {
+                id: 1,
+                firstName: 'test',
+                lastName: 'test',
+                civility: Civility.M,
+                phoneNumber: '094393904350',
+                email: 'deofke@gmail.com',
+                address: null,
+            };
+            // @ts-ignore
+            jest.spyOn(service, 'create');
+            service.create = jest.fn(() => contactToCreate);
+            expect(await controller.create(contactToCreate)).toBe(contactToCreate);
+        });
     });
-  });
 
-  describe('remove', () => {
-    it('should delete a contact', async () => {
-      const contactToDelete = {
-        id: 1,
-        firstName: 'test',
-        lastName: 'test',
-        civility: Civility.M,
-        phoneNumber: '094393904350',
-        email: 'deofke@gmail.com',
-        address: null,
-      };
-      // @ts-ignore
-      jest.spyOn(service, 'remove').mockImplementation(() => contactToDelete);
-      expect(await controller.remove(contactToDelete.id.toString())).toBe(contactToDelete);
+    describe('findOne', () => {
+        it('should find a contact', async () => {
+            const contactToFind = {
+                id: 1,
+                firstName: 'test',
+                lastName: 'test',
+                civility: Civility.M,
+                phoneNumber: '094393904350',
+                email: 'deofke@gmail.com',
+                address: null,
+            };
+            jest.spyOn(service, 'findOne');
+            service.findOne = jest.fn(() => contactToFind);
+            expect(await controller.findOne(contactToFind.id.toString())).toBe(contactToFind);
+        });
     });
-  });
+
+    describe('update', () => {
+        it('should update a contact', async () => {
+            const id = '10';
+            const contactToUpdate = {
+                firstName: 'test',
+                lastName: 'test',
+                civility: Civility.M,
+                phoneNumber: '094393904350',
+                email: 'deofke@gmail.com',
+                address: null,
+            };
+            jest.spyOn(service, 'update');
+            service.update = jest.fn(() => contactToUpdate);
+            const updatedContact = await controller.update(id, contactToUpdate);
+            expect(updatedContact).toBe(contactToUpdate);
+        });
+    });
+
+    describe('remove', () => {
+        it('should delete a contact', async () => {
+            const id = '10';
+            const contactToDelete = {
+                id: 1,
+                firstName: 'test',
+                lastName: 'test',
+                civility: Civility.M,
+                phoneNumber: '094393904350',
+                email: 'deofke@gmail.com',
+                address: null,
+            };
+            jest.spyOn(service, 'remove');
+            service.remove = jest.fn(() => contactToDelete);
+            await controller.remove(id);
+            expect(service.remove).toBeCalledWith(+id);
+        });
+    });
 
 });
 
