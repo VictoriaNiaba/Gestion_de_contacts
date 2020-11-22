33f23bcdba98f9cf30df500f7ddc2451f7342210
VictoriaNiaba <niaba.victoria@gmail.com>
2020-11-22 17:13:45 +0100

    Amélioration des informations globales de la spécification

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,33 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Filtre global qui applique les règles de validation pour chaque route
+  app.useGlobalPipes(new ValidationPipe());
+
   const options = new DocumentBuilder()
-    .setTitle('Gestion de Contact')
-    .setDescription('Application API de Gestion de Contact')
+    .setTitle('Phone Book')
+    .setDescription(
+      `This API is developed as part of the Software 
+    Engineering course at Aix-Marseille University. It is public and
+    allows to manage a set of contacts (creation, deletion, update, reading ...).`,
+    )
     .setVersion('1.0')
-    .addTag('contacts')
+    .setContact('Phone book administrators', null, 'phonebook.m2ild@gmail.com')
+    .setExternalDoc(
+      'Source code on Gitlab',
+      'https://gitlab.com/farhane.hafsa/phone-book',
+    )
+    .setLicense('MIT License', 'http://opensource.org/licenses/MIT')
+    .addServer('http://localhost:{port}', 'Local server', {
+      port: { description: 'API port', default: 3000 },
+    })
+    .addServer('https://phone-book-staging.herokuapp.com', 'Staging server')
+    .addServer('https://phone-book-prod.herokuapp.com', 'Production server')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('swagger', app, document);
 
-  // Filtre global qui applique les règles de validation pour chaque route
-  app.useGlobalPipes(new ValidationPipe());
-
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
